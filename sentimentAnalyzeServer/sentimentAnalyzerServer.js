b4a2e3b26e2071737c74ff09a4c4af2d7b579b1c
111y <igor189@bk.ru>
2021-05-19 21:00:58 +0000

    comment

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,5 +1,26 @@
 const express = require('express');
-const app = new express();
+const dotenv = require('dotenv');
+dotenv.comfig();
+
+function getNLUInstance(){
+    let api_key = sentimentAnalyzeServer.env.API_KEY;
+    let api_url = sentimentAnalyzeServer.env.API_URL;
+
+    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } = require('ibm-watson/auth');
+
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+        version: '2020-08-01',
+        authenticator: new IamAuthenticator({
+            apikey: api_key,
+        }),
+        serviceUrl: api_url,
+    });
+    return naturalLanguageUnderstanding;
+}
+
+
+/*const app = new express();
 
 app.use(express.static('client'))
 
